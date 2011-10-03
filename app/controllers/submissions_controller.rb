19996c06beb3f09c095f87a648b478415a8c0333
Andrés Mejía <andmej@gmail.com>
2011-10-02 21:43:33 -0500

    Fixing permissions on Submissions#index.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,4 +1,5 @@
 class SubmissionsController < ApplicationController
+  load_and_authorize_resource  
   def index
     @contest = Contest.find(params[:contest_id])
     @submissions = @contest.submissions.order("submitted_at desc").all
